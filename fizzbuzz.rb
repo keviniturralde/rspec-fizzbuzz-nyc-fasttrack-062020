c1839728f8483f9522f8571be728dea778cad2f5
Kevin Iturralde <keviniturralde3@gmail.com>
2020-06-24 17:17:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 &&  int % 5 == 0
-    puts "fizzbuzz"
+    puts "Fizzbuzz"
   elsif int % 5 == 0
-      puts "buzz"
+      puts "Buzz"
     elsif int % 3 == 0
-      puts "fizz"
+      puts "Fizz"
     end
   end
